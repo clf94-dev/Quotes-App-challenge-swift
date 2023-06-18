9aecdecd38797ea5665e74db0e0215c85ae58f86
clf94-dev <clf94@hotmail.es>
2023-06-18 09:14:16 +0200

    Initial Commit

---
+++ Quotes App challenge/Quotes_App_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_App_challengeApp.swift
+//  Quotes App challenge
+//
+//  Created by Carmen Lucas on 18/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_App_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
